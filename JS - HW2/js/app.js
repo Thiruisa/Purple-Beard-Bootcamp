59bdde55f470d8cd1945c0d1afc41f4b6237f8a1
Thiruisa Puvanenthirarasa <thiruisa@hotmail.co.uk>
2021-12-06 10:56:32 +0000

    hw2 final update

--- JS - HW2/js/app.js
+++ JS - HW2/js/app.js
@@ -2,7 +2,7 @@
 
 // Display the current date
 function todayDate(){
-    const dateVar;
+    var dateVar;
     // current date
     dateVar = new Date();
     // alert with todays date will appear with button is clicked
@@ -42,14 +42,24 @@ function bkgColour(){
 
 // create a site with 3 images in clickable cards
 
-// once clicked something happens i.e. images changes, you could set the background, information appear under the image
+// once clicked something happens i.e. images changes, information appear under the image
 
-function imgChange(){
+function imgChange(value){
+    // change image on click
+    document.getElementById("img").src = "https://images.unsplash.com/photo-1609675764298-dfa140691a62?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1094&q=80";
+    
+    // click image for text to appear
+    const div = document.getElementById("div")
+    var text = "";
 
-let img = document.getElementById("img");
+    if (value == 1) text += "Blue and Red Painting";
 
-img.addEventListener("click", function(){
-    console.log("clicked");
-})
+    div.innerHTML = text;
 
+    const div1 = document.getElementById("div1")
+    var text = "";
+
+    if (value == 2) text += "Pink and Grey Painting";
+
+    div1.innerHTML = text;
 }
